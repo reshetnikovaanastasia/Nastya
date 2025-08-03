37f491ff568b2ee3734c6d8201bb3d5861208f3e
анастасия решетникова <reshetnikovaanastasia04@gmail.com>
2025-08-04 02:20:29 +0500

    corection

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,11 @@
-from typing import Iterable
+from typing import Any
 import datetime
 
 
-def filter_by_state(list_data_dictionaries: Iterable[list], state='EXECUTED') -> Iterable[list]:
-    """ Принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED'). Функция возвращает
-    новый список словарей, содержащий только те словари, у которых ключ 'state' соответствует указанному значению"""
+def filter_by_state(list_data_dictionaries: Any, state = 'EXECUTED') -> Any:
+    """Принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED'). Функция
+    возвращает новый список словарей, содержащий только те словари, у которых ключ 'state' соответствует
+    указанному значению"""
     new_list_data = []
     for data_dictionary in list_data_dictionaries:
         if data_dictionary['state'] == state:
@@ -12,8 +13,8 @@ def filter_by_state(list_data_dictionaries: Iterable[list], state='EXECUTED') ->
     return new_list_data
 
 
-def sort_by_date(list_data_dictionaries: Iterable[list], data_key="date", direction=True) -> Iterable[list]:
-    """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание). Функция должна
-    возвращать новый список, отсортированный по дате (date)"""
-    return sorted(list_data_dictionaries, key=lambda x: datetime.datetime.strptime(x[data_key], '%Y-%m-%dT%H:%M:%S.%f'),
-                  reverse=direction)
+def sort_by_date(list_data_dictionaries: Any, data_key = "date", direction = True) -> Any:
+    """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
+    Функция должна возвращать новый список, отсортированный по дате (date)"""
+    return sorted(list_data_dictionaries, key = lambda x: datetime.datetime.strptime(x[data_key],
+                '%Y-%m-%dT%H:%M:%S.%f'), reverse = direction)
