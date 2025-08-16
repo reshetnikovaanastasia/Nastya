9fcf528176d18ebc7e5d26f83824afde3a567361
анастасия решетникова <reshetnikovaanastasia04@gmail.com>
2025-08-17 02:25:58 +0500

    add card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -80,6 +80,9 @@ transactions = (
 
 
 def filter_by_currency(transactions, selected_currency):
+    """ Принимает на вход список словарей, представляющих транзакции.
+    Функция должна возвращать итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной"""
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == selected_currency:
             yield transaction
@@ -91,9 +94,26 @@ def filter_by_currency(transactions, selected_currency):
 
 
 def transaction_descriptions(transactions):
+    """Принимает список словарей с транзакциями и возвращает
+    описание каждой операции по очереди"""
     for transaction in transactions:
         yield transaction["description"]
+
 
 # descriptions = transaction_descriptions(transactions)
 # for _ in range(5):
 #     print(next(descriptions))
+
+def card_number_generator(beginning, end):
+    """ Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где
+    X — цифра номера карты. Генератор может сгенерировать номера карт в заданном диапазоне
+    от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    Генератор должен принимать начальное и конечное значения для генерации диапазона номеров."""
+    start = str(beginning)
+    for i in range(beginning, end + 1):
+        yield f"{start.zfill(16)[:4]} {start.zfill(16)[4:9]} {start.zfill(16)[9:13]} {start.zfill(16)[13:]}"
+        beginning += 1
+        start = str(beginning)
+
+# for card_number in card_number_generator(1, 5):
+#     print(card_number)
