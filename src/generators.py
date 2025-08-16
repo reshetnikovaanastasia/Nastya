0514ece8bdeddc9f09e93689f5be0efd18c8a572
анастасия решетникова <reshetnikovaanastasia04@gmail.com>
2025-08-17 03:05:11 +0500

    add test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,84 +1,3 @@
-transactions = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-)
-
-
 def filter_by_currency(transactions, selected_currency):
     """ Принимает на вход список словарей, представляющих транзакции.
     Функция должна возвращать итератор, который поочередно выдает транзакции,
@@ -88,21 +7,12 @@ def filter_by_currency(transactions, selected_currency):
             yield transaction
 
 
-# usd_transactions = filter_by_currency(transactions, "USD")
-# for _ in range(3):
-#     print(next(usd_transactions))
-
-
 def transaction_descriptions(transactions):
     """Принимает список словарей с транзакциями и возвращает
     описание каждой операции по очереди"""
     for transaction in transactions:
         yield transaction["description"]
 
-
-# descriptions = transaction_descriptions(transactions)
-# for _ in range(5):
-#     print(next(descriptions))
 
 def card_number_generator(beginning, end):
     """ Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где
@@ -111,9 +21,6 @@ def card_number_generator(beginning, end):
     Генератор должен принимать начальное и конечное значения для генерации диапазона номеров."""
     start = str(beginning)
     for i in range(beginning, end + 1):
-        yield f"{start.zfill(16)[:4]} {start.zfill(16)[4:9]} {start.zfill(16)[9:13]} {start.zfill(16)[13:]}"
+        yield f"{start.zfill(16)[:4]} {start.zfill(16)[4:8]} {start.zfill(16)[8:12]} {start.zfill(16)[12:]}"
         beginning += 1
         start = str(beginning)
-
-# for card_number in card_number_generator(1, 5):
-#     print(card_number)
---
+++ tests/test_generators.py
@@ -0,0 +1,39 @@
+import pytest
+
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+
+def test_filter_by_currency(transactions):
+    usd_transactions = filter_by_currency(transactions, "USD")
+    assert next(usd_transactions) == {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572',
+                                      'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code':
+                                          'USD'}}, 'description': 'Перевод организации',
+                                      'from': 'Счет 75106830613657916952',
+                                      'to': 'Счет 11776614605963066702'}
+    assert next(usd_transactions) == {'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878',
+                                      'operationAmount': {'amount': '79114.93',
+                                                          'currency': {'name': 'USD', 'code': 'USD'}},
+                                      'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542',
+                                      'to': 'Счет 75651667383060284188'}
+
+
+def test_transaction_descriptions(transactions):
+    descriptions = transaction_descriptions(transactions)
+    assert next(descriptions) == "Перевод организации"
+    assert next(descriptions) == "Перевод со счета на счет"
+    assert next(descriptions) == "Перевод со счета на счет"
+    assert next(descriptions) == "Перевод с карты на карту"
+    assert next(descriptions) == "Перевод организации"
+
+
+def test_card_number_generator():
+    a = card_number_generator(1, 5)
+    assert next(a) == "0000 0000 0000 0001"
+    assert next(a) == "0000 0000 0000 0002"
+    assert next(a) == "0000 0000 0000 0003"
+    assert next(a) == "0000 0000 0000 0004"
+    assert next(a) == "0000 0000 0000 0005"
+    b = card_number_generator(11111111, 11111113)
+    assert next(b) == "0000 0000 1111 1111"
+    assert next(b) == "0000 0000 1111 1112"
+    assert next(b) == "0000 0000 1111 1113"
