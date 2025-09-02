3a5808321363ceb8db2f0949192ded442b566c64
анастасия решетникова <reshetnikovaanastasia04@gmail.com>
2025-09-02 21:39:40 +0500

    add utils.py

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import json
+import os
+from json import JSONDecodeError
+
+
+def load_transaction_data(file_path):
+    """Функция принимает на вход путь до JSON-файла и возвращает
+    список словарей с данными о финансовых транзакциях"""
+    try:
+        try:
+            if os.path.getsize(file_path) == 0:
+                return []
+            with open(file_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+        except JSONDecodeError:
+            return []
+        return data
+    except FileNotFoundError:
+        return []
+
+
+# Необходимо указать абсолютный путь к файлу
+print(load_transaction_data('/data/operations.json'))
